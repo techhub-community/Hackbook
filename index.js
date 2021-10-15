e9d45ac3ff7916224e141e1c068edf84cbd1ef42
Ammyy9908 <sb78639@gmail.com>
2021-10-15 15:49:44 +0530

    optional commit

--- index.js
+++ index.js
@@ -194,7 +194,6 @@ const getDiscord = async () => {
 
 getDiscord()
   .then((discord_data) => {
-    console.log(discord_data);
     const { members, instant_invite } = discord_data;
     document.querySelector(".discord_btn").setAttribute("href", instant_invite);
     document.querySelector(".discord-count").textContent = members.filter(
@@ -204,20 +203,3 @@ getDiscord()
   .catch((e) => {
     console.log(e);
   });
-
-setInterval(() => {
-  getDiscord()
-    .then((discord_data) => {
-      console.log(discord_data);
-      const { members, instant_invite } = discord_data;
-      document
-        .querySelector(".discord_btn")
-        .setAttribute("href", instant_invite);
-      document.querySelector(".discord-count").textContent = members.filter(
-        (member) => member.status === "online"
-      ).length;
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-}, 30000);
